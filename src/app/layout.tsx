0502b91fbe82f64af705acbfcdebc1135de332de
claudio <claudio@gamecam.se>
2025-02-02 10:29:26 +0100

    Create Redux Provider
    Since Next.js uses app router, I created Provider and  wrap the application with the Redux provider.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ReduxProvider } from "@/redux/Provider";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -25,9 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
   );
