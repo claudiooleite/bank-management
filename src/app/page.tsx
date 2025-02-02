60054f66463235c51729b5af2da59cf9fb21b53f
claudio <claudio@gamecam.se>
2025-02-02 18:35:31 +0100

    Create Card Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,40 @@
 "use client";
 
+import { useState } from "react";
+import { useTranslations } from "next-intl";
+
 import AccountList from "../components/AccountList";
 import AccountForm from "../components/AccountForm";
 import TransferForm from "../components/TransferForm";
-import { useState } from "react";
-import { useTranslations } from "next-intl";
+import Card from "@/components/Card";
 
 export default function Home() {
   const t = useTranslations();
   const [editingAccount, setEditingAccount] = useState(null);
 
   return (
-    <main>
+    <>
       <h1>{t("title")}</h1>
-      <AccountForm
-        existingAccount={editingAccount}
-        clearEdit={() => setEditingAccount(null)}
-      />
-      <TransferForm />
-      <AccountList onEdit={setEditingAccount} />
+
+      <Card>
+        <AccountForm
+          existingAccount={editingAccount}
+          clearEdit={() => setEditingAccount(null)}
+        />
+      </Card>
+
+      <Card>
+        <TransferForm />
+      </Card>
+
+      <Card>
+        <AccountList onEdit={setEditingAccount} />
+      </Card>
+
       <h1 className="text-4xl font-bold text-blue-700">Tailwind is Working!</h1>
       <button className="mt-4 px-4 py-2 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600">
         Click Me
       </button>
-    </main>
+    </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,16 +3,12 @@
 import React from "react";
 
 interface CardProps {
-  title: string;
-  description?: string;
   children?: React.ReactNode;
 }
 
-export default function Card({ title, description, children }: CardProps) {
+export default function Card({ children }: CardProps) {
   return (
-    <div
-      aria-labelledby={`card-title-${title}`}
-      className="border rounded-lg p-4 shadow-md bg-white max-w-md">
+    <div className="border rounded-lg p-4 shadow-md bg-white w-fit m-3">
       {children}
     </div>
   );
