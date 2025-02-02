bf42c05573ec10df3fba81d4ac290786f4783c5f
claudio <claudio@gamecam.se>
2025-02-02 16:15:33 +0100

    Install next-intl for Localization
    Create Translation Files and update Provider with en as default language.

--- src/redux/Provider.tsx
+++ src/redux/Provider.tsx
@@ -2,7 +2,19 @@
 
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { NextIntlClientProvider } from "next-intl";
+import { useState } from "react";
+import en from "../locales/en.json";
+import es from "../locales/es.json";
+
+const messages = { en, es };
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  const [locale, setLocale] = useState("en");
+
+  return (
+    <NextIntlClientProvider messages={messages[locale]} locale={locale}>
+      <Provider store={store}>{children}</Provider>
+    </NextIntlClientProvider>
+  );
 }
